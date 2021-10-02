0e39c8f205095499bdc748c503356fa9e4ba372c
Gavin D'souza <gavin18d@gmail.com>
2021-10-02 18:15:19 +0530

    feat(wip): Generate Profile Pic from Photos cluster

--- photos/photos/doctype/person/person.py
+++ photos/photos/doctype/person/person.py
@@ -1,10 +1,48 @@
-# -*- coding: utf-8 -*-
 # Copyright (c) 2020, Gavin D'souza and contributors
 # For license information, please see license.txt
 
-from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+import json
+import cv2
+from photos.utils import get_image_path, image_resize
 
 class Person(Document):
-    pass
+    def validate(self):
+        if not self.get("person_image"):
+            self.set_profile_pic(save=False)
+
+    def set_profile_pic(self, save=True):
+        if self.user:
+            # get user profile pic
+            self.person_image = frappe.db.get_value("User", self.user, "user_image")
+        else:
+            # get random pic from ROI and set the pic here
+            self.person_image = self.generate_profile_pic()
+
+        if save:
+            self.save()
+
+    def generate_profile_pic(self):
+        result = frappe.get_all(
+            "ROI",
+            filters={"person": self.person_name},
+            limit_page_length=1,
+            order_by="creation desc",
+            fields=["image", "location"],
+            as_list=True
+        )
+        if result:
+            _image, location = result[0]
+            image_path = get_image_path(
+                frappe.db.get_value("File", _image, "file_url")
+            )
+            top, right, bottom, left = json.loads(location)
+            # TODO: fix cropping logic
+            img_cropped = cv2.imread(image_path)[left:right, top:bottom]
+            # TODO: create a file doc for temp profile pic and link it to self.person_image
+            # create and save image in the tmp dir? frappe will probs copy the contents inside?
+            cv2.imwrite("./temp_crop.jpg", img_cropped)
+        else:
+            # random gravatar??
+            pass
